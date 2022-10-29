9b26263829cb41e04d0a854e1b4529cffb9bfd02
ShimaaHamdi <108419926+ShimaaHamdi@users.noreply.github.com>
2022-10-29 07:43:40 -0500

    Update main.js
    Signed-off-by: ShimaaHamdi <108419926+ShimaaHamdi@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -76,12 +76,4 @@ var swiper = new Swiper(".review-slider", {
   },
 });
 
-// function loader() {
-//   document.querySelector(".loader-container").classList.add("fade-out");
-// }
-
-// function fadeOut() {
-//   setInterval(loader, 1000);
-// }
-
-// window.onload = fadeOut;
+ 
